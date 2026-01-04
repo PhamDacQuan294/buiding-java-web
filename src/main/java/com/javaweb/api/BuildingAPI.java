effea3f7c7ffb8177637c495371d57b7474b0cf7
quan <quan93909@gmail.com>
2026-01-04 14:29:21 +0700

    lan 1

---
+++ src/main/java/com/javaweb/api/BuildingAPI.java
@@ -0,0 +1,33 @@
+package com.javaweb.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javaweb.model.BuildingDTO;
+import com.javaweb.service.BuildingService;
+
+import customexception.FieldRequiredException;
+
+@RestController
+public class BuildingAPI {
+	@Autowired
+	private BuildingService buildingService;
+	
+	@GetMapping(value = "/api/building/")
+	public List<BuildingDTO> getBuilding(@RequestParam(name="name", required = false) String name,
+										@RequestParam(name="districtId", required = false) Long districtid,
+										@RequestParam(name="typeCode", required = false) List<String> typeCode) {
+		List<BuildingDTO> result = buildingService.findAll(name, districtid);
+		return result;
+	}
+
+	public void valiDate(BuildingDTO buildingDTO) {
+		if (buildingDTO.getName() == null || buildingDTO.getName().equals("")) {
+			throw new FieldRequiredException("name is null");
+		}
+	}
+}
